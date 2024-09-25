1131225f787303787be6f8fa43f5b5da8400e6f2
cmb2333 <cmb2333@nau.edu>
2024-09-25 15:33:59 -0700

    Configured basics of joke app

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const axios = require('axios');
+const app = express();
+const port = 3000;
+
+//https://medium.com/@mottammal1993/building-a-simple-web-app-with-ejs-axios-and-express-js-263ade0467c7
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.render('index', { joke: null });
+});
+
+app.post('/results', (req, res) => {
+    const word = req.body.word;
+
+    axios.get(`https://v2.jokeapi.dev/joke/Any?contains=${word}`)
+        .then(response => {
+
+            console.log(response.data);
+
+            const joke = response.data.type === 'single'
+                ? response.data.joke
+                : `${response.data.setup} - ${response.data.delivery}`;
+            res.render('results', { joke });
+        })
+        .catch(error => {
+            console.error('Error fetching joke:', error.message);
+            res.render('results', { joke: 'Could not fetch a joke.' });
+        });
+});
+
+app.listen(port, () => console.log(`Server running at http://localhost:${port}`));
+
